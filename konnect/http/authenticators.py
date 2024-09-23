60646d7a3a6f5876fdab53a2b6c6e9c9709c6850
Dom Sekotill <dom.sekotill@kodo.org.uk>
2024-09-23 23:13:44 +0100

    Make authenticator hooks' arguments positional only
    This change should hopefully make implementations simpler and allow
    a choice of names for arguments.

--- konnect/http/authenticators.py
+++ konnect/http/authenticators.py
@@ -1,4 +1,4 @@
-# Copyright 2023  Dom Sekotill <dom.sekotill@kodo.org.uk>
+# Copyright 2023-2024  Dom Sekotill <dom.sekotill@kodo.org.uk>
 
 """
 Authentication handlers for adding auth data to requests and implementing auth flows
@@ -20,7 +20,7 @@ class AuthHandler(Protocol):
 	Abstract definition of authentication handlers' entrypoints
 	"""
 
-	async def prepare_request(self, request: CurlRequest) -> None:
+	async def prepare_request(self, request: CurlRequest, /) -> None:
 		"""
 		Process a request instance before the request is enacted
 
@@ -29,7 +29,7 @@ class AuthHandler(Protocol):
 		before the request.  Any such flow SHOULD use the request's session.
 		"""
 
-	async def process_response(self, request: CurlRequest, response: Response) -> Response:
+	async def process_response(self, request: CurlRequest, response: Response, /) -> Response:
 		"""
 		Examine a response to a request and perform any follow-up actions
 
