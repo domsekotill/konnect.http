bf97b68d1a331de59bb4334182b7c31b5b00c9ba
Dom Sekotill <dom.sekotill@kodo.org.uk>
2026-02-23 10:08:58 +0000

    Export ContentType from the top-level

--- konnect/http/__init__.py
+++ konnect/http/__init__.py
@@ -1,7 +1,8 @@
-# Copyright 2023-2025  Dom Sekotill <dom.sekotill@kodo.org.uk>
+# Copyright 2023-2026  Dom Sekotill <dom.sekotill@kodo.org.uk>
 
 from .request import Hook as Hook
 from .request import Method as Method
 from .request import Request as Request
+from .response import ContentType as ContentType
 from .response import Response as Response
 from .session import Session as Session
